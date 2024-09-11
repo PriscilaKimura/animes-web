bff3583617b30268393af8bd40d5c4db79694e22
Priscila Kimura <141864300+PriscilaKimura@users.noreply.github.com>
2024-09-11 11:16:10 -0300

    Formatando a data

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -11,32 +11,35 @@ interface CardProps {
   
   const Card: React.FC<CardProps> = ({ anime, onDelete }) => {
     const imageSrc = animeImages[anime.id]; // Utilize o ID do anime para obter a imagem
-  
-    return (
-      <div>
-        <h2>{anime.title}</h2>
-        {imageSrc ? (
-          <img 
-            src={imageSrc} 
-            alt={anime.title} 
-            style={{ width: '200px', height: 'auto' }} 
-          />
-        ) : (
-          <p>Imagem não disponível</p>
-        )}
-        <p>{anime.description}</p>
-        <p>{anime.completed ? 'Completo' : 'Em andamento'}</p>
-        <p>Data de lançamento: {anime.releaseDate}</p>
-        <p>Tipo: {anime.type === 'filme' ? 'Filme' : 'Série'}</p>
-        <p>Ranking: {anime.ranking}</p> 
-        <Button to={`/update/${anime.id}`}>Editar</Button>
-        <DeleteButton onClick={onDelete}>Excluir</DeleteButton>
-      </div>
-    );
-  };
-  
+
+  // Convertendo o formato da data
+  const formattedDate = new Date(anime.releaseDate).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+
+  return (
+    <div>
+      <h2>{anime.title}</h2>
+      {imageSrc ? (
+        <img 
+          src={imageSrc} 
+          alt={anime.title} 
+          style={{ width: '200px', height: 'auto' }} 
+        />
+      ) : (
+        <p>Imagem não disponível</p>
+      )}
+      <p>{anime.description}</p>
+      <p>{anime.completed ? 'Completo' : 'Em andamento'}</p>
+      {/* Mostrando a data formatada */}
+      <p>Data de lançamento: {formattedDate}</p>
+      <p>Tipo: {anime.type === 'filme' ? 'Filme' : 'Série'}</p>
+      <p>Ranking: {anime.ranking}</p> 
+      <Button to={`/update/${anime.id}`}>Editar</Button>
+      <DeleteButton onClick={onDelete}>Excluir</DeleteButton>
+    </div>
+  );
+};
   export default Card;
-
-
-
-
